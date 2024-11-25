6103c26a84c59ccc285f89f2a9e6748d95b33c68
wstndp <147555138+wstndp@users.noreply.github.com>
2024-11-25 21:31:01 +0000

    Add total_order_value

--- customers.sql
+++ customers.sql
@@ -4,6 +4,7 @@ SELECT
   c.email,
   min(o.created_at) AS first_order_at,
   COUNT (DISTINCT o.id) AS number_of_orders
+  sum(`analytics-engineers-club.coffee_shop.orders`.total) as total_order_value
 FROM 
   `analytics-engineers-club.coffee_shop.orders` o
 INNER JOIN
