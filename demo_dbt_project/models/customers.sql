09bcf021f8407d6da5b45dd9198f479b8a03f3cd
wstndp <147555138+wstndp@users.noreply.github.com>
2024-11-28 20:03:44 +0000

    update sql

---
+++ demo_dbt_project/models/customers.sql
@@ -0,0 +1,12 @@
+select 
+  customers.id,
+  customers.name,
+  customers.email,
+  min(orders.created_at) AS first_order_at,
+  count(distinct orders.id) AS number_of_orders,
+  sum(orders.total) as total_order_value
+from `analytics-engineers-club.coffee_shop.orders`
+inner join `analytics-engineers-club.coffee_shop.customers` on id = orders.customer_id
+group by 1,2,3
+order by first_order_at 
+limit 5
